76f942b985006811b95107253fe025593089b95c
IsabelTizon <tizonarranz@gmail.com>
2024-06-13 10:49:59 +0100

    Map into the array scrimba iterview challenges components

--- src/app/scrimba/page.js
+++ src/app/scrimba/page.js
@@ -1,5 +1,23 @@
-import React from "react";
+import { challenges } from "../../components/challenges";
+
+export const metadata = {
+	title: "Scrimba Interview Challenges",
+	description:
+		"60 interview challenges from Scrimba to study different methods",
+};
 
 export default function Scrimba() {
-	return <div>page</div>;
+	console.log("challenges", challenges);
+	return (
+		<div>
+			<div>Hello World!</div>
+			<div className="flex flex-col">
+				{challenges.map((challenge) => (
+					<div className="" key={challenge.id}>
+						<h3>{challenge.name}</h3>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
