5fa18356d99c9e380578818876fdddde8eb52644
IsabelTizon <tizonarranz@gmail.com>
2024-06-13 16:39:20 +0100

    details challenge page created

---
+++ src/app/scrimba/[challengeDetails]/page.js
@@ -0,0 +1,63 @@
+import { challenges } from "../../../components/challenges";
+
+export default function ChallengeDetails({ params }) {
+	const { challengeDetails } = params;
+
+	const challenge = challenges[challengeDetails];
+
+	console.log(
+		"=================================================="
+	);
+	/*
+	DESESTRUCTURACION EN JS:
+	https://www.freecodecamp.org/news/destructuring-in-javascript/
+
+	*/
+	console.log("params => ", params);
+	/*
+	console.log("params => ", params);
+	params = EL OBJETO Y SU VALOR
+	params =>  { challengeDetails: '0' }	
+	*/
+
+	console.log(
+		"=================================================="
+	);
+	console.log("challengeDetails =>", challengeDetails);
+	/*
+	console.log("challengeDetails =>", challengeDetails);
+	challengeDetails = SOLO EL VALOR
+	(challengeDetails) => 0;	
+	*/
+	console.log(
+		"=================================================="
+	);
+	console.log(
+		"challenges[challengeDetails]===>",
+		challenges[challengeDetails]
+	);
+	/*
+	console.log(
+		"challenges[challengeDetails]===>",
+		challenges[challengeDetails]
+	);		challenges= TODAS LOS OBJENTOS CON SUS PROPIEDADES Y VALORES
+	challenges[challengeDetails] = LAS PROPIEDADES Y LOS VALORES DEL OBJETO ESTABLECIDO
+	*/
+	console.log(
+		"=================================================="
+	);
+	console.log("All Challenges", challenges);
+	/*
+	console.log("All Challenges", challenges);
+	challenges= TODAS LOS OBJENTOS CON SUS PROPIEDADES Y VALORES
+	*/
+
+	return (
+		<div>
+			Hello World!
+			<h1 className="" key={challenge.id}>
+				{challenge.name}
+			</h1>
+		</div>
+	);
+}
