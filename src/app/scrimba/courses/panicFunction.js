8678c83f5e55178fbb89e3e24630241af4ed73f1
IsabelTizon <tizonarranz@gmail.com>
2024-06-19 12:25:59 +0100

    Deleted console.log on Panic Function

--- src/app/scrimba/courses/panicFunction.js
+++ src/app/scrimba/courses/panicFunction.js
@@ -1,6 +1,3 @@
 export default function panicFunction(str) {
 	return str.split("").join(" 😱 ").toUpperCase() + "!";
 }
-
-console.log(panic("I'm almost out of coffee"));
-console.log(panic("winter is coming"));
