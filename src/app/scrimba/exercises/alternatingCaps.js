e057530cf96d731dd6ebf5c6ca3cea64225302b6
IsabelTizon <tizonarranz@gmail.com>
2024-06-24 17:52:49 +0100

    Alternating Caps exercise with forEach()

--- src/app/scrimba/exercises/alternatingCaps.js
+++ src/app/scrimba/exercises/alternatingCaps.js
@@ -1,20 +1,46 @@
+// function  with for loop
+// export function alternatingCaps(str) {
+// 	const altCapsArray = str.toLowerCase().split("");
+// 	console.log(altCapsArray);
+
+// 	for (let i = 0; i < altCapsArray.length; i++) {
+// 		if (i % 2 === 0) {
+// 			console.log("===> par -", altCapsArray[i]);
+
+// 			altCapsArray[i] = altCapsArray[i].toUpperCase();
+// 			console.log(
+// 				"===> Even (par) Capitalised -",
+// 				altCapsArray[i].toUpperCase()
+// 			);
+// 		} else {
+// 			console.log("===> Odd (impar)", altCapsArray[i]);
+// 		}
+// 		console.log("final: ", altCapsArray.join(""));
+// 	}
+// 	return altCapsArray.join("");
+// }
+
+// function  with forEach
+// En JavaScript, el método .forEach() es utilizado para ejecutar una función sobre cada elemento de un array. Este método es útil para realizar operaciones sobre los elementos de un array sin la necesidad de utilizar un bucle for tradicional.
+
 export function alternatingCaps(str) {
-	const altCapsArray = str.toLowerCase().split("");
-	console.log(altCapsArray);
+	console.log("str: ", str);
+	console.log("lowerCaseStr: ", str.toLowerCase());
+	console.log(
+		"lettersArray: ",
+		str.toLowerCase().split("")
+	);
+	let lettersArray = str.toLowerCase().split("");
 
-	for (let i = 0; i < altCapsArray.length; i++) {
-		if (i % 2 === 0) {
-			console.log("===> par -", altCapsArray[i]);
-
-			altCapsArray[i] = altCapsArray[i].toUpperCase();
+	lettersArray.forEach((element, index) => {
+		if (index % 2 === 0) {
+			console.log(`===> even element ${index}: ${element}`);
 			console.log(
-				"===> Even (par) Capitalised -",
-				altCapsArray[i].toUpperCase()
+				`===> even element Upper case ${element.toUpperCase()}`
 			);
-		} else {
-			console.log("===> Odd (impar)", altCapsArray[i]);
+			lettersArray[index] = element.toUpperCase();
+			console.log("new array", lettersArray);
 		}
-		console.log("final: ", altCapsArray.join(""));
-	}
-	return altCapsArray.join("");
+	});
+	return "newArray", lettersArray.join(" ");
 }
