d0c0b586efb2d2669dffbc4dda579178532755fd
IsabelTizon <tizonarranz@gmail.com>
2024-07-26 17:00:53 +0100

    isAnagram exercise done!

--- src/app/scrimba/exercises/isAnagram.js
+++ src/app/scrimba/exercises/isAnagram.js
@@ -1,14 +1,27 @@
 export function isAnagram(str1, str2) {
-	// Minusculas con el metodo toLowerCase()
-	const lowerstr1 = str1.toLowerCase();
-	const lowerstr2 = str2.toLowerCase();
-	console.log(`1) str1: ${lowerstr1} str2: ${lowerstr2}`);
-
 	// Primero de todo ver si tienen la misma longitud
-	if (lowerstr1.length == lowerstr2.length) {
+	if (str1.length == str2.length) {
 		console.log(
-			`2) str1 (${lowerstr1}) and str2 (${lowerstr2}) have the same length`
+			`2) str1 (${str1}) and str2 (${str2}) have the same length`
 		);
+		// Minusculas con el metodo toLowerCase()
+		let newStr1 = str1
+			.toLowerCase()
+			.split("")
+			.sort()
+			.join("");
+		let newStr2 = str2
+			.toLowerCase()
+			.split("")
+			.sort()
+			.join("");
+
+		console.log(
+			`1) lower str1: ${newStr1} lower str2: ${newStr2}`
+		);
+
+		if (newStr1 === newStr1) {
+			return `RETURN =============================> ${str1} is an anagram of ${str2}`;
+		}
 	}
-	return `RETURN =============================>  ${lowerstr1} and ${lowerstr2}`;
 }
