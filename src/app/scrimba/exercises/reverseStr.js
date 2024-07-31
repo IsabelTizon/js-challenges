750d5a084f6a2884710695da700d8e144f33dfa5
IsabelTizon <tizonarranz@gmail.com>
2024-07-31 05:26:09 +0100

    more corrections for reverseStr exercise

--- src/app/scrimba/exercises/reverseStr.js
+++ src/app/scrimba/exercises/reverseStr.js
@@ -1,8 +1,8 @@
-//Reverse is an array method and you can not using on a str. What I first need to do is pass my str to an array with the split() method
+//Reverse is an array method and you can not using on a str. What I first need to do is pass my str into an array with the split() method
 export function reverseStr(str) {
 	return str.split("").reverse().join("");
 }
 
 export function reverseStrArray(arr) {
-	return arr.map((str) => str.split("").reverse().join(""));
+	return arr.map((str) => reverseStr(str));
 }
