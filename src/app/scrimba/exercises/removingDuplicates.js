07965e21d12473f9ed91efbae4a28a85dd4f09bf
IsabelTizon <tizonarranz@gmail.com>
2024-08-01 14:41:30 +0100

    removingDuplicates exercise done!!

--- src/app/scrimba/exercises/removingDuplicates.js
+++ src/app/scrimba/exercises/removingDuplicates.js
@@ -1,4 +1,19 @@
 export function removingDuplicates() {
 	console.log(`1) Removing Duplicates characters Exercise`);
-	// return <div>removingDuplicates</div>;
+
+	const password =
+		"9338dsabbbadjdjdj2sdfdfdf282ff8fdsd888ss8cfgfg332q23";
+
+	// I was inisialing the variable realPassword with const
+	//BUT
+	//cannot reassign to a variable declared with `const`!!!!!!!
+
+	let realPassword = "";
+	password.split("").map((char) => {
+		if (!realPassword.includes(char)) {
+			realPassword += char;
+		}
+	});
+	console.log(`2) passwordArray: ${realPassword}`);
+	return realPassword;
 }
