c92e8620d9ca1190432af9d32a1b248f015f34f2
IsabelTizon <tizonarranz@gmail.com>
2024-08-02 13:03:46 +0100

    countChars exercise object created in challengesArray Components

--- src/components/challengesArray.js
+++ src/components/challengesArray.js
@@ -367,15 +367,17 @@ export const challenges = [
 	},
 	{
 		id: 10,
-		name: "",
-		descriptions: [" "],
-		examples: [
-			"Example input: ''",
-			"Example output: ",
-			"Example input: ''",
-			"Example output: ",
+		name: "countChars. How often do the letters in your name repeat? ",
+		descriptions: [
+			"Write a function that counts how many times each letter of your name occurs. Your function should take in your first and last name and return an object where the keys are each character in your name, and the value is how many times that character appears in your name.",
+			"Your function should NOT count spaces and should not be case sensitive (a lowercase t and a capital T should be considered the same character).",
+			"function countChars(str){}",
 		],
-		tester: [""],
+		examples: [
+			"Example input: 'Peggy Porth'",
+			"Example output: '{p: 2, e: 1, g: 2, y: 1, o: 1, r: 1, t: 1, h: 1}'",
+		],
+		tester: ["console.log(countChars('Peggy Porth'));"],
 		methods: ["", "", ""],
 		explanations: ["", "", "", ""],
 		github: "",
