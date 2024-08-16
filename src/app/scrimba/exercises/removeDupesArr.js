d731373c6183817057074b8cd387d4fc008b1377
IsabelTizon <66060248+IsabelTizon@users.noreply.github.com>
2024-08-16 12:30:49 +0100

    removeDupesArr2 done and explain in ChallengesArray Components
    removeDupesArr2 resolve with the filter method

--- src/app/scrimba/exercises/removeDupesArr.js
+++ src/app/scrimba/exercises/removeDupesArr.js
@@ -3,7 +3,27 @@ export function removeDupesArr(arr) {
 	console.log(`2) array: ${arr}`);
 	return [...new Set(arr)];
 }
-
 export function removeDupesArr2(arr) {
 	console.log(`3) Hello Remove Duplicates From Array 2 Exercise!!!!!`);
+	return arr.filter(
+		(item, index) =>
+			// console.log(`4) item: ${item}, index: ${index}`)
+			// console.log(`4) item: ${arr.indexOf(item)}`)
+			/*
+			4) item: 0 ===> 🥓 bacon
+			4) item: 0
+			4) item: 2 ===> 🍳 eggs
+			4) item: 3 ===> 🫑 green peppers
+			4) item: 4 ===> 🧀 cheese
+			4) item: 5 ===> 🌶️ hot sauce
+			4) item: 0
+			4) item: 7 ===> 🥦 broccoli
+			4) item: 4
+			4) item: 7
+			4) item: 5
+*/
+			arr.indexOf(item) === index
+	);
+
+	// console.log(`4) arr.indexOf(item):`);
 }
