4f177cd4a6fdc4821e1744733be025e8854f1591
IsabelTizon <66060248+IsabelTizon@users.noreply.github.com>
2024-12-07 21:28:10 +0100

    New array of objects to set different courses on the home page.

---
+++ src/components/homeArr.js
@@ -0,0 +1,16 @@
+export const homeCourses = [
+	{
+		id: 0,
+		href: "/scrimba",
+		name: "Interview Challenges",
+		intro:
+			"help to you for your essential tech interview preparation pack! Practice solving problems and honing the skills you need to succeed in a frontend coding interview.",
+	},
+	{
+		id: 0,
+		href: "/learnBasics",
+		name: "Learn JS Basics",
+		intro:
+			"Learn the basics of JavaScript by solving 140+ interactive coding challenges. Along the way you will build a game, a browser extension, and even a mobile app!",
+	},
+];
