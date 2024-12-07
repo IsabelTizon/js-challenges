2e56c7158146dd524bcb01aff2a9304b21a12529
IsabelTizon <66060248+IsabelTizon@users.noreply.github.com>
2024-12-07 22:36:44 +0100

    scrimba folder  renamed to scrimbaChallengesJS

--- src/components/arrays/homeArr.js
+++ src/components/arrays/homeArr.js
@@ -1,14 +1,14 @@
 export const homeCourses = [
 	{
 		id: 0,
-		href: "/scrimba",
+		href: "/scrimbaChallengesJS",
 		name: "Interview Challenges",
 		intro:
 			"help to you for your essential tech interview preparation pack! Practice solving problems and honing the skills you need to succeed in a frontend coding interview.",
 	},
 	{
 		id: 0,
-		href: "/learnBasics",
+		href: "/scrimbaLearnBasics",
 		name: "Learn JS Basics",
 		intro:
 			"Learn the basics of JavaScript by solving 140+ interactive coding challenges. Along the way you will build a game, a browser extension, and even a mobile app!",
