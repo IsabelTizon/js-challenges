589502c43d3eb3625cbb0117475bd8d0a70fcd7f
IsabelTizon <66060248+IsabelTizon@users.noreply.github.com>
2024-12-10 19:36:33 +0100

    iteratingOverArrays page updated with a map to iterate the array component

--- src/app/iteratingOverArrays/page.js
+++ src/app/iteratingOverArrays/page.js
@@ -1,4 +1,5 @@
 import CodeBlock from "@/components/CodeBlock";
+import { iteratingArrMethods } from "@/components/arrays/iteratingArrMethods";
 
 export const metadata = {
 	title: "Iterating Over Arrays",
@@ -9,70 +10,32 @@ export const metadata = {
 // rfc(React Fuctional Component)
 
 export default function page() {
-	const foorLoop = `
-    const array = [1, 2, 3, 4, 5];
-
-    for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-    }
-    `;
-
-	const foorOf = `
-    const array = [1, 2, 3, 4, 5];
-
-	for (const element of array) {
-		console.log(element);
-		}
-    `;
-
-	const forEach = `
-    const array = [1, 2, 3, 4, 5];
-    
-	array.forEach((element, index) => {
-    console.log(\`Índice: \${index}, Elemento: \${element}\`);
-    });
-    `;
-
-	const map = `
-    const array = [1, 2, 3, 4, 5];
-    
-    const squared = array.map((num) => num ** 2);
-
-    console.log(squared); // [1, 4, 9, 16, 25]
-    `;
-
-	const filter = `
-    const array = [1, 2, 3, 4, 5];
-
-    const even = array.filter((num) => num % 2 === 0);
-
-    console.log(even); // [2, 4]
-    `;
-
-	const reduce = `
-    const array = [1, 2, 3, 4, 5];
-
-    const sum = array.reduce((acc, curr) => acc + curr, 0);
-    
-    console.log(sum); // 15
-    `;
-
 	return (
-		<div className="p-8">
+		<div className="">
 			<h1>Iterating Over Arrays</h1>
-
-			<h2>Bucle for clásico</h2>
-			<CodeBlock code={foorLoop} language="javascript" />
-			<h2>for ... of</h2>
-			<CodeBlock code={foorOf} language="javascript" />
-			<h2>forEach</h2>
-			<CodeBlock code={forEach} language="javascript" />
-			<h2>map</h2>
-			<CodeBlock code={map} language="javascript" />
-			<h2>map</h2>
-			<CodeBlock code={filter} language="javascript" />
-			<h2>map</h2>
-			<CodeBlock code={reduce} language="javascript" />
+			<div className="flex flex-row">
+				{iteratingArrMethods.map((method) => (
+					<div key={method.id} className="p-4 border rounded">
+						<h2>{method.h2}</h2>
+						<p>{method.p}</p>
+						<CodeBlock code={method.code} language="javascript" />
+					</div>
+				))}
+			</div>
 		</div>
 	);
+}
+
+{
+	/* // <h2>for ... of</h2>
+// <CodeBlock code={foorOf} language="javascript" />
+// <h2>forEach</h2>
+// <p></p>
+// <CodeBlock code={forEach} language="javascript" />
+// <h2>map</h2>
+// <CodeBlock code={map} language="javascript" />
+// <h2>filter</h2>
+// <CodeBlock code={filter} language="javascript" />
+// <h2>map</h2>
+// <CodeBlock code={reduce} language="javascript" /> */
 }
