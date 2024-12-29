baab463155b6d94ff1f819e38e72685595fbe460
Isabel Tizon Arranz <tizonarranz@gmail.com>
2024-12-29 15:10:41 +0100

    isValid updated
    se ha suprimido el if y se ha retornado el valor directamente para que haya menos líneas de código y esté de esta manera más limpio

--- src/app/scrimbaChallengesJS/exercises/isValid.js
+++ src/app/scrimbaChallengesJS/exercises/isValid.js
@@ -7,14 +7,10 @@ export function isValid(str) {
 
   //Iteration of arr with filter()
   const iterationArr = strArr.filter((item) => {
-    if (
-      ((item.startsWith("(") && item.endsWith(")"))) && item.length > 2 ||
+    return (
+      (item.startsWith("(") && item.endsWith(")") && item.length > 2) ||
       (!item.includes("(") && !item.includes(")"))
-    ) {
-      console.log(`2) item iterated on Arr:${item}`);
-      return `3) item returned on Arr:${item}`;
-    }
+    );
   });
-  console.log(`4) Value of the item iterated on Arr:${iterationArr}`);
   return `5) item returned on Arr:${iterationArr}`;
 }
